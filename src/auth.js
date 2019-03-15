4ad0e1acae0f6827a73280967386ed37746644ab
Ben Parisot <me@benparisot.com>
2019-03-15 16:15:00 -0700

    get login auth working

---
+++ src/auth.js
@@ -0,0 +1,23 @@
+import { auth, usersRef } from './firebase.js';
+
+const ui = new firebaseui.auth.AuthUI(auth);
+ui.start('#login', {
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: './',
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    callbacks: {
+        signInSuccessWithAuthResult(authResult) {
+            const user = authResult.user;
+            usersRef.child(user.uid)
+                .set({
+                    uid: user.uid,
+                    displayName: user.displayName,
+                    photoURL: user.photoURL
+                });
+            return true;
+        }
+    }
+});
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: 'AIzaSyBqfe4jjrZ8WTXkSftzkRliEYGFiZSGgBA',
+    authDomain: 'talk-giphy.firebaseapp.com',
+    databaseURL: 'https://talk-giphy.firebaseio.com',
+    projectId: 'talk-giphy',
+    storageBucket: 'talk-giphy.appspot.com',
+    messagingSenderId: '916515783240'
+};
+
+export const app = firebase.initializeApp(config);
+export const auth = firebase.auth();
+const db = firebase.database();
+export const usersRef = db.ref('user');
