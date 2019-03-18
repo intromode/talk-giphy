98119ac0b7d39fb137930606da355a653df1dbca
ollinieminen <otn987@gmail.com>
2019-03-18 10:40:11 -0700

    set up translate and guess pages, hardcode translate.html

---
+++ data/gif-list.js
@@ -0,0 +1,16 @@
+export const results = {
+    data: [
+        {
+            id: 'FP2OpdSFCR7hK',
+            url: 'https://giphy.com/gifs/kitten-cute-adorable-FP2OpdSFCR7hK',
+        },
+        {
+            id: 'wHc92cHADhpLi',
+            url: 'https://giphy.com/gifs/kitten-cat-wHc92cHADhpLi',
+        },
+        {
+            id: 'FP2OpdSFCR7hK',
+            url: 'https://giphy.com/gifs/kitten-cute-adorable-FP2OpdSFCR7hK',
+        }
+    ]
+};
