5545bb656103f5ce7e86dfb2bf99a789e1a8ec17
ollinieminen <otn987@gmail.com>
2019-03-18 12:08:40 -0700

    write read message from query test function

--- test/hash-components.test.js
+++ test/hash-components.test.js
@@ -9,9 +9,16 @@ function writeToQuery(currentQuery, message) {
     return searchParams.toString();
 }
 
+function readMessageFromQuery(currentQuery) {
+    const searchParams = new URLSearchParams(currentQuery);
+    const message = searchParams.get('q');
+    const queryValues = {
+        message: message
+    };
+    return queryValues;
+}
+
 test('write to query', assert => {
-
-
     //arrange
     const currentQuery = '';
     const expected = 'q=dog';
@@ -22,3 +29,12 @@ test('write to query', assert => {
     assert.equal(result, expected);
 });
 
+test('read message from query', assert => {
+    const currentQuery = 'q=dog';
+    const expected = {
+        message: 'dog'
+    };
+    const result = readMessageFromQuery(currentQuery);
+
+    assert.deepEqual(result, expected);
+});
