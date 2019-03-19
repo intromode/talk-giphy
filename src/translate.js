aa71fc59451918ee62973cf62442c02e14ed8480
ollinieminen <otn987@gmail.com>
2019-03-18 17:24:31 -0700

    sort works!!!!

--- src/translate.js
+++ src/translate.js
@@ -15,56 +15,20 @@ window.addEventListener('hashchange', () => {
     const currentQuery = window.location.hash.slice(1);
     const queryValues = readMessageFromQuery(currentQuery);
     const splitMessage = queryValues.message.split(' ');
-    console.log('split message', splitMessage);
     const numberMessages = splitMessage.length;
-    console.log('number of messages', numberMessages);
-
     const responseArray = [];
-
-
+    
     for(let i = 0; i < numberMessages; i++) {
-
-        console.log('split message', splitMessage[i]);
         const url = makeApiURL(splitMessage[i]);
         fetch(url)
             .then(response => response.json())
             .then(result => {
                 result['order'] = i;
                 responseArray.push(result);
-                console.log('responseArray', responseArray);
-            });
-        
-        const correctedGifOrder = responseArray.sort((a, b) => {
-            if(a.order === b.order) {
-                return 0;
-            }
-            if(a.order > b.order) {
-                return -1;
-            }
-            return 1;
-        });
-        console.log('newOrder!!!', correctedGifOrder);
+                responseArray.sort((a, b) => a.order - b.order);
+                        
+            });    
     }
-
-
-
-
-
-
-    //splitMessage.forEach(message => {
-    //  const url = makeApiURL(message);
-    //fetch(url)
-    //  .then(response => response.json())
-    //.then(results => {
-    //  console.log('**message**', message, '**results**', results);
-    //loadGifs(results);
-    //});
-
-    //});
-
-
-
-
 });
 
 
